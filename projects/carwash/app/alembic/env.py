7289f76ea2baeaa407194a43103bf219dda360cf
Wereker <werkermatvey@gmail.com>
2025-02-02 23:54:09 +0700

    Fix almbic migrations

--- projects/carwash/app/alembic/env.py
+++ projects/carwash/app/alembic/env.py
@@ -7,9 +7,9 @@ from sqlalchemy.ext.asyncio import async_engine_from_config
 
 from alembic import context
 
-from core.config import settings
-from db import Base
-from models import *
+from app.core.config import settings
+from app.db import Base
+from app.models import *
 
 # this is the Alembic Config object, which provides
 # access to the values within the .ini file in use.
